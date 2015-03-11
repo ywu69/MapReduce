91b1bb4b9641796dd9d2099300a3d191fe42fac2
raulpitz <pengzhan@PENGtekiMacBook-Pro.local>
2015-03-10 20:48:06 -0700

    aa

--- mr_master.py
+++ mr_master.py
@@ -2,6 +2,7 @@ __author__ = 'pengzhan'
 import sys
 
 import zerorpc
+import os
 import gevent
 
 class Master(object):
@@ -34,8 +35,74 @@ class Master(object):
     def register(self, ip, port):
         gevent.spawn(self.register_async, ip, port)
 
-    def set_job(self, split_size, num_reducers, input_filename, output_filename_base):
-        pass
+    def set_worker_state(self, ip, port, state):
+        gevent.spawn(self.set_worker_state_async, ip, port, state)
+
+    def set_worker_state_async(self, ip, port, state):
+        print 'set map state'
+        self.workers[(ip,port)] = (state, self.workers[(ip,port)][1])
+
+    def set_job(self,job_name, split_size, num_reducers, input_filename, output_filename_base):
+        gevent.spawn(self.setJob_async, job_name, split_size, num_reducers, input_filename, output_filename_base)
+
+    def setJob_async(self,job_name, split_size, num_reducers, input_filename, output_filename_base):
+        #split input file
+        splited_files = [];
+
+        #Align map tasks to workers
+        print 'MAP phase'
+        i = 0;
+        procs = []
+        for w in self.workers:
+            if self.workers[w][0] == 'READY':
+                print 'let him do map'
+                gevent.spawn(self.workers[w][1].do_map, job_name)#, splited_files[i])
+
+        #Wait until all map tasks done
+        print 'Wait until all map tasks done'
+        #while True:
+        #    mapDone = True
+        #    for w in self.workers:
+        #        if self.workers[w][0] != 'MAPDONE':
+        #            mapDone = False
+        #    if mapDone:
+        #        break
+
+        #Reduce
+        #print 'REDUCE phase'
+        #for w in self.workers:
+        #    proc = gevent.spawn(self.workers[w][1].do_reduce, job_name)
+        #    procs.append(proc)
+
+        #wait for reduce done
+        #print 'Wait until all reduce tasks done'
+        #while True:
+        #    reduceDone = True
+        #    for w in self.workers:
+        #        if self.workers[w][0] != 'REDUCEDONE':
+        #            reduceDone = False
+        #    if reduceDone:
+        #        break
+
+        #collect
+
+    def split_file(self):
+        fileSize = os.path.getsize(data_dir)
+        print(fileSize)
+        subfile_size = int(fileSize/5)
+        index = 1
+        with open(data_dir) as inputfile:
+            current_size = 0
+            outputfile = open('sub_inputfile_' + str(index) + '.txt', 'w')
+            for line in inputfile:
+                current_size += len(line)
+                print(current_size)
+                outputfile.write(line)
+                if index < 5 and current_size >= subfile_size:
+                    current_size = 0
+                    index = index + 1
+                    outputfile.close()
+                    outputfile = open('sub_inputfile_' + str(index) + '.txt', 'w')
 
     def do_job(self, nums):
         n = len(self.workers)
@@ -61,9 +128,11 @@ class Master(object):
         return sum([int(p.value) for p in procs])
 
 if __name__ == '__main__':
-    port = 4242#sys.argv[1]
-    data_dir = "\\"#sys.argv[2]
-    master_addr = 'tcp://0.0.0.0:' + str(port)
-    s = zerorpc.Server(Master(data_dir))
-    s.bind(master_addr)
-    s.run()
+    #port = 4242#sys.argv[1]
+    data_dir = "inputfile.txt"#sys.argv[2]
+    #master_addr = 'tcp://0.0.0.0:' + str(port)
+    #s = zerorpc.Server(Master(data_dir))
+    #s.bind(master_addr)
+    #s.run()
+    m = Master(data_dir)
+    m.split_file()
--- mr_worker.py
+++ mr_worker.py
@@ -5,7 +5,13 @@ import socket
 import gevent
 
 class Worker(object):
-    def __init__(self):
+    def __init__(self, master_addr, worker_ip, worker_port):
+        self.master_addr = master_addr
+        self.worker_port = worker_port
+        self.worker_ip = worker_ip
+        self.c = zerorpc.Client()
+        self.c.connect("tcp://"+master_addr)
+        self.c.register(worker_ip, worker_port)
         gevent.spawn(self.controller)
         pass
 
@@ -17,6 +23,13 @@ class Worker(object):
     def ping(self):
         print('[Worker] Ping from Master')
 
+    def do_map(self, job_name):
+        print 'Doing MAP '+ self.worker_ip + ':'+self.worker_port
+        self.c.set_worker_state(self.worker_ip, self.worker_port, 'MAPDONE')
+
+    def do_reduce(self, job_name, mapresults):
+        self.c.set_worker_state(self, self.worker_ip, self.worker_port, 'REDUCEDONE')
+
     def do_work(self, nums):
         nums = [int(n) for n in nums]
         gevent.sleep(2)
@@ -26,11 +39,8 @@ if __name__ == '__main__':
     master_addr = "127.0.0.1:4242"#sys.argv[1];
     worker_ip = "127.0.0.1"
     worker_port = sys.argv[1]
-    s = zerorpc.Server(Worker())
+    s = zerorpc.Server(Worker(master_addr,worker_ip,worker_port))
     s.bind('tcp://' + worker_ip+":"+worker_port)
-    c = zerorpc.Client()
-    c.connect("tcp://"+master_addr)
-    c.register(worker_ip, worker_port)
     s.run()
 
     #print socket.gethostbyname(socket.gethostname())
